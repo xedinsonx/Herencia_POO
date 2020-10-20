817a97fdf00d68e81ff265534b5667f4a32d704b
xedinsonx <xedinsonx2@gmail.com>
2020-10-20 17:39:42 -0500

    Herencia_POO

---
+++ src/Herencia/Estudiante.java
@@ -0,0 +1,23 @@
+
+package Herencia;
+
+
+public class Estudiante extends Persona{
+    
+    private int codigoEstudiante;
+    private float notaFinal;
+    
+    public Estudiante(String nombre,String apellido,int edad,int codigoEstudiante,float notaFinal){
+            super(nombre,apellido,edad);
+            this.codigoEstudiante = codigoEstudiante;
+            this.notaFinal = notaFinal;
+    }
+    public void mostradDatos(){
+        System.out.print("Nombre: "+getNombre()+
+                "\nApellido: "+getApellido()+
+                "\nEdad: "+getEdad()+
+                "\nCodigo Estudiante: "+codigoEstudiante+
+                "\nNota Final: "+notaFinal);
+            
+}
+}
---
+++ src/Herencia/Principal.java
@@ -0,0 +1,15 @@
+
+package Herencia;
+
+
+public class Principal {
+    
+    public static void main(String [] args){
+        
+        Estudiante estudiante = new Estudiante("Edixon","Villon",35,443450601,17);
+            
+            estudiante.mostradDatos();
+        }
+    }
+    
+
